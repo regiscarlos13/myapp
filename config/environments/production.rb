456cf33714133c89f8902c205e408f23076de3a8
Your Name <regiscarlos13@gmail.com>
2019-08-26 23:03:46 -0300

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
    config.require_master_key = true
    config.read_encrypted_secrets = true
 
+  config.require_master_key
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
