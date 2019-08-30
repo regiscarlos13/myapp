46a1f056c8b249c97f3014ae492596ea297f2b2b
Your Name <regiscarlos13@gmail.com>
2019-08-29 22:26:31 -0300

    correcao

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/regiscarlos13/myapp.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/myapp/#{fetch :application}"
+set :deploy_to, "/var/www/#{fetch :application}"
 
 set :branch, "master"
 # Default value for :format is :airbrussh.
